c69e782bdf8f2b82caa1a5a31cc11422a3f65780
Andrey Murashkin <andrey@phpteam.pro>
2016-12-13 17:03:48 +0800

    logbag updates

--- src/Model/Bag/Base.php
+++ src/Model/Bag/Base.php
@@ -3,6 +3,7 @@
 namespace Murich\Logbag\Model\Bag;
 
 use Murich\Logbag\Model\Ray;
+use SixZeroClub\SharedPHPLibrary\Kohana\Arr;
 
 abstract class Base {
 
@@ -37,6 +38,23 @@ abstract class Base {
         $values['ray'] = $this->ray->getRay();
 
         return json_encode($values);
+    }
+
+    public static function prepareObject($object)
+    {
+        if (is_object($object) && !($object instanceof \JsonSerializable)) {
+            $prepared = Arr::dismount($object);
+            $prepared['class'] = get_class($object);
+        } else {
+            $prepared = $object;
+        }
+
+        return $prepared;
+    }
+
+    public function getRay()
+    {
+        return $this->ray;
     }
 
     /**
